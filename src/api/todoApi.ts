1c778a2d0579283963d181f9594b150ef0203a4d
Fernando Sousa <fssonca@gmail.com>
2024-11-17 19:01:36 -0300

    feat: add Axios interceptor to handle 401 errors and redirect to login

--- src/api/todoApi.ts
+++ src/api/todoApi.ts
@@ -1,45 +1,58 @@
 import axios from "axios";
 import { API_BASE_URL } from "../utils/constants";
 
+function getAuthHeaders() {
+  const jwtToken = localStorage.getItem("authToken");
+  return {
+    Authorization: `Bearer ${jwtToken}`,
+    "Content-Type": "application/json",
+  };
+}
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: getAuthHeaders(),
+});
+
+// interceptor to handle responses
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // If the response is successful, simply return it
+    return response;
+  },
+  (error) => {
+    // If a response error occurs
+    if (error.response && error.response.status === 401) {
+      // Clear the token if it exists
+      localStorage.removeItem("authToken");
+
+      // Redirect the user to the login/register page
+      window.location.href = "/";
+    }
+    // Reject the promise with the error object
+    return Promise.reject(error);
+  }
+);
+
+export async function fetchTodos() {
+  return axiosInstance.get("/todos");
+}
+
 export async function saveTodo(todo: {
   name: string;
   description: string;
   priority: number;
   dueDate: string | null;
 }) {
-  const jwtToken = localStorage.getItem("authToken");
-
-  return axios.post(`${API_BASE_URL}/todos`, todo, {
-    headers: {
-      Authorization: `Bearer ${jwtToken}`,
-      "Content-Type": "application/json",
-    },
-  });
+  return axiosInstance.post("/todos", todo);
 }
 
 export async function completeTodo(todoId: string, completed: boolean) {
-  const jwtToken = localStorage.getItem("authToken");
-
-  return axios.post(
-    `${API_BASE_URL}/todos/complete`,
-    { todoId, completed },
-    {
-      headers: {
-        Authorization: `Bearer ${jwtToken}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  return axiosInstance.post("/todos/complete", { todoId, completed });
 }
 
 export async function deleteTodo(todoId: string) {
-  const jwtToken = localStorage.getItem("authToken");
-
-  return axios.delete(`${API_BASE_URL}/todos`, {
+  return axiosInstance.delete("/todos", {
     data: { todoId },
-    headers: {
-      Authorization: `Bearer ${jwtToken}`,
-      "Content-Type": "application/json",
-    },
   });
 }
